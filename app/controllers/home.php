f36e348297edeea29b47600cd903eac0ed54cbac
Albino-Fox <albinofox9@gmail.com>
2023-06-18 19:43:05 +1000

    +composer +eloquent and db test

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,10 +4,15 @@ class Home extends Controller
 {
     public function index($name = ''){
         echo("default params of home/index activated");
-
-        $user = $this->model('User');
-        $user->name = $name;
         
-        $this->view('home/index', ['name' => $user->name]);
+        $this->view('home/index', ['name' => $name]);
+    }
+
+    public function create($login = '', $pass = ''){
+        User::create([
+            'login' => $login,
+            'password' => $pass
+        ]);
+        echo($login . ' ' . $pass);
     }
 }
