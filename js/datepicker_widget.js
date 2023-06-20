d8d1a92c324b8d780c1eaeb50a20b53870a89a87
Albino-Fox <albinofox9@gmail.com>
2023-06-20 14:51:25 +1000

    got rid out of trash and used cdn links

--- js/datepicker_widget.js
+++ js/datepicker_widget.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
   $('#datepicker').datepicker({
     format: 'yyyy-mm-dd',
     autoclose: true,
-    todayHighlight: true,
-    startDate: new Date(), // Optional: Set the minimum selectable date to today
-    endDate: '+30d' // Optional: Set the maximum selectable date to 30 days from today
+    todayHighlight: false
   });
 });
