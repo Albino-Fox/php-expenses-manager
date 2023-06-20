0e881019bc266d833e84ceb7d429fdb9af60bb0c
Albino-Fox <albinofox9@gmail.com>
2023-06-20 14:43:29 +1000

    somehow added bootstrap-datepicker calendar

--- app/views/expenses/create.php
+++ app/views/expenses/create.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet/less" type="text/css" href="/less/bootsrap-datepicker/datepicker.less" />
     <title>Add expenses</title>
 </head>
 <body>
@@ -40,11 +41,17 @@
         </select>
         <label for="amount">Amount:</label>
         <input type="text" id="amount" name="amount">
+        <label for="date">Date:</label>
+        <input type="text" id="datepicker" name="selectedDate" readonly>
         <input type="submit" value="Create Expense">
     </form>
 
     <script src="https://code.jquery.com/jquery-latest.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script src="/js/less.js" type="text/javascript"></script>
+    <script src="/js/bootstrap-datepicker/bootstrap-datepicker.js" type="text/javascript"></script>
+    <script src="/js/datepicker_widget.js" type="text/javascript"></script>
+    
 
 </body>
 </html>
