1dbb6a34e19b915646c8df0d6ac177e1aa81bc63
Albino-Fox <albinofox9@gmail.com>
2023-06-22 20:03:44 +1000

    added bootstrap and style.css

--- app/views/partials/navbar.php
+++ app/views/partials/navbar.php
@@ -1,12 +1,11 @@
-<nav>
+<nav class="navbar">
         <ul>
             <li><a href="/home">Home</a></li>
             <li><a href="/expenses/createExpense">Add Expense</a></li>
             <li><a href="/expenses">View Expenses</a></li>
             <li><a href="/login">Login</a></li>
             <li><a href="/register">Register</a></li>
-            </br> <!-- <- rewrite later? -->
-            <?php if(isset($_SESSION['user_id'])) echo('Logged as: ' . $_SESSION['nickname']);?> 
+            <?php if(isset($_SESSION['user_id'])) echo('<li>Logged as: ' . $_SESSION['nickname'] . '</li>');?> 
             <?php if(isset($_SESSION['user_id'])) echo('<li><a href="/logout">Logout</a></li>');?>
         </ul>
 </nav>
