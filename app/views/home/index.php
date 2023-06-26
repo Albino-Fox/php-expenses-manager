b223a2b6c8dfbe91b455d210cdc8401cf5400ec2
Albino-Fox <albinofox9@gmail.com>
2023-06-26 19:10:02 +1000

    primitive design added

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
-    <title>Home</title>
+    <title>BankruptBuddy</title>
 </head>
 <body>
     <!--NAVBAR_PLACEHOLDER-->
-    <div class="container mt-5">
+    <!-- <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-8">
                 <div class="jumbotron">
@@ -21,7 +21,46 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
+
+    <section class="hero text-center text-white d-flex align-items-center justify-content-center" style="background: url('/static/images/hero.jpg') center center no-repeat; background-size: cover; height: 50vh;">
+        <div>
+            <h1>Добро пожаловать в BankruptBuddy!</h1>
+            <p class="lead">Мы поможем вам управлять вашими расходами</p>
+        </div>
+    </section>
+
+    <!-- Features Section -->
+    <section class="features container mt-5">
+        <div class="row">
+            <!-- Feature 1 -->
+            <div class="col-lg-4 text-center">
+                <img src="/static/images/feature1.jpg" alt="Feature 1" class="img-fluid rounded-circle mb-4"> <!-- Replace with your image -->
+                <h2>Отслеживайте финансы</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <!-- Feature 2 -->
+            <div class="col-lg-4 text-center">
+                <img src="/static/images/feature2.jpg" alt="Feature 2" class="img-fluid rounded-circle mb-4"> <!-- Replace with your image -->
+                <h2>undefined</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <!-- Feature 3 -->
+            <div class="col-lg-4 text-center">
+                <img src="/static/images/feature3.jpg" alt="Feature 3" class="img-fluid rounded-circle mb-4"> <!-- Replace with your image -->
+                <h2>undefined</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Call to Action Section -->
+    <section class="cta bg-light text-dark text-center p-4 mt-5">
+        <h2>Готовы взять контроль над своими финансами?</h2>
+        <a href="/expenses/createExpense" class="btn btn-primary btn-lg mt-3">Записать транзакцию</a>
+    </section>
+
+
     <!--SCRIPTS_PLACEHOLDER-->
 </body>
 </html>
